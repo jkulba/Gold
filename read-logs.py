bc90aa73a1f67fb692e8a3a986b65a5ff0128450
Jim Kulba <jkulba@gmail.com>
2021-12-22 17:58:12 -0800

    Add read-logs file

--- read-logs.py
+++ read-logs.py
@@ -10,31 +10,38 @@ Generates specified number of files with ASCII characters.
 
 NO_FILES = 500
 FILE_SIZE = 500
-LOG_DIR = '/apps/ProFile/EXPORT/lock_manager/J/LSW'
+#LOG_DIR = '/apps/ProFile/EXPORT/lock_manager/J/LSW'
+LOG_DIR = '/home/jim/Projects/Gold/'
 
 def main():
     print("Starting read log files")
 
-    ROOT_DIR = os.getcwd()
-    print("Current working directory: ", ROOT_DIR)
+    print("Current working directory: ", LOG_DIR)
 
-    if os.path.exists(ROOT_DIR):
-        os.chdir(ROOT_DIR)
+    if os.path.exists(LOG_DIR):
+        os.chdir(LOG_DIR)
 
-    read_file()
+    read_file(LOG_DIR)
 
     pass
 
-def read_file():
-    for filename in os.listdir():
-        print(filename)
-        with open(filename) as fd:
+def read_file(LOG_DIR):
+    for root, dirs, files in os.walk('.', topdown=False, onerror=None, followlinks=False):
+        for filename in files:
+            print(os.path.join(LOG_DIR, filename))
+            fd = os.open(filename, os.O_RDWR|os.O_CREAT)
             fo = os.fdopen(fd, "r")
-            os.lseek(fd, 0, 0)
-            string = os.read(fd, 500)
-            print(string)
-            print("Current I/O pointer position :%d" % fo.tell())
-            fo.close()
+
+
+    # for filename in os.listdir():
+    #     print(filename)
+    #     with open(filename) as fd:
+    #         fo = os.fdopen(fd, "r")
+    #         os.lseek(fd, 0, 0)
+    #         string = os.read(fd, 500)
+    #         print(string)
+    #         print("Current I/O pointer position :%d" % fo.tell())
+    #         fo.close()
     pass
 
 if __name__=="__main__":
