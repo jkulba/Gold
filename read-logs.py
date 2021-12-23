b44d49fc0a4d6d8250ef2d00b292aa14e8cb19aa
Jim Kulba <jkulba@gmail.com>
2021-12-22 18:32:52 -0800

    final tweaks to use fdopen

--- read-logs.py
+++ read-logs.py
@@ -3,19 +3,15 @@
 import sys, os, random, string, time
 
 """
-Generates specified number of files with ASCII characters.
+Reads files from specified directory.
 :param filename: unique filename
 :param size: size in bytes
 """
 
-NO_FILES = 500
-FILE_SIZE = 500
 #LOG_DIR = '/apps/ProFile/EXPORT/lock_manager/J/LSW'
 LOG_DIR = '/home/jim/Projects/Gold/'
 
 def main():
-    print("Starting read log files")
-
     print("Current working directory: ", LOG_DIR)
 
     if os.path.exists(LOG_DIR):
@@ -27,25 +23,22 @@ def main():
 
 def read_file(LOG_DIR):
     for filename in os.listdir():
-        print(filename)
+        path = os.path.join(LOG_DIR, filename)
+        if os.path.isdir(path):
+            continue
+        print("FILENAME: " + filename)
+        fd = os.open(filename, os.O_RDWR)
+        fo = os.fdopen(fd, "r")
+        os.lseek(fd, 0, 0)
+        string = os.read(fd, 500)
+        print("CONTENT")
+        print(string)
+        print(" ")
+        print("Current I/O pointer position: %d" %fo.tell())
+        fo.close()
+        print("---------")
 
-
-    # for root, dirs, files in os.walk('.', topdown=False, onerror=None, followlinks=False):
-    #     for filename in files:
-    #         print(os.path.join(LOG_DIR, filename))
-    #         fd = os.open(filename, os.O_RDWR|os.O_CREAT)
-    #         fo = os.fdopen(fd, "r")
-
-
-    # for filename in os.listdir():
-    #     print(filename)
-    #     with open(filename) as fd:
-    #         fo = os.fdopen(fd, "r")
-    #         os.lseek(fd, 0, 0)
-    #         string = os.read(fd, 500)
-    #         print(string)
-    #         print("Current I/O pointer position :%d" % fo.tell())
-    #         fo.close()
+    print("Done")
     pass
 
 if __name__=="__main__":
